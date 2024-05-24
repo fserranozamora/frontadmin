631a61974474d3e52eac8819a81980d54cb495cc
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-05-24 11:40:05 -0500

    Update config.js
    baseURL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: {
        // baseURL: "http://localhost:5000"
-        baseURL: "https://backadmin-opbz.onrender.com/"
+        baseURL: "https://backadmin-opbz.onrender.com"
     }
 }
